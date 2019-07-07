bdfd02c57e73fa795136eb5174ec696958134019
Jorge Horacio Cue Cantu <jorge.cue@gmail.com>
2019-07-07 08:16:24 -0500

    Initial commit

---
+++ src/main/java/com/example/gbpsvc/GbpsvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.gbpsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbpsvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbpsvcApplication.class, args);
+    }
+
+}
